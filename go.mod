f4ffdbfc437306acc26620d3891b6384fc93e746
Farzon <af.nuriddinov@gmail.com>
2021-10-28 18:00:42 +0300

    инициализирован модуль

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/NAziz21/banktypes
+
+go 1.17
---
+++ pkg/bank/types/types.go
@@ -0,0 +1,52 @@
+package types
+
+
+// Представляет собой денежную сумму в минимальных единицах (копейки и т.д.)
+type Money int64
+
+// Представляет код валюты
+type Currency string
+
+// Category представляет собой категорию, в которой был совершен платеж
+type Category string
+
+// Коды валют
+
+const (
+	TJS Currency = "TJS"
+	RUB Currency = "RUB"
+	USD Currency = "USD"
+)
+
+// Представляет номер карты
+type PAN string
+
+// Представляет информацию о платежной карте
+type Card struct {
+	ID         int
+	PAN        PAN
+	Balance    Money
+	MinBalance Money
+	Currency   Currency
+	Color      string
+	Name       string
+	Active     bool
+}
+
+
+
+type Payment struct {
+	ID int 
+	Amount Money
+	Category Category
+}
+
+
+type PaymentSource struct {
+	Type    string // card
+	Number  string // номер вида '5058 хххх хххх 8888' 
+	Balance Money // баланс в дирамах
+}
+
+
+
